ded9fff9c941e2b92086b1eda14c8c28c8bfd00b
SaliimH <sh.hemmar@gmail.com>
2022-04-21 20:10:09 +0200

    changement du gif & reglage de lanimation & ajout du text intro

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,18 +1,33 @@
-import React from "react";
+/* eslint-disable no-multi-str */
+import React, { useState } from "react";
+import { useTypewriter } from "react-simple-typewriter";
 
-import Milas from '../../Assets/Milas.gif';
+import MilasA from "../../Assets/Milas.gif";
+import MilasB from "../../Assets/Milas2.png";
 import "./HomePage.css";
 
 const HomePage = () => {
-  return(
+  const [image, setImage] = useState(MilasA);
 
-    <div>
-        <img className="milas" src={Milas} alt="signmove" />
-    <p>
-      coucou c'est moi !!!
-      </p>
-      </div>
-    );
+  setTimeout(() => {
+    setImage(MilasB);
+  }, 17000);
+
+  const { text } = useTypewriter({
+    words: [
+      "Bonjour et bienvenu dans mon portfolio Je m'appelle Salim et je suis un jeune développeur \
+      web & passionné de nouvelles technologies. J'ai suivi une formation de 5 mois aupres de la wild code school\
+     et je suis actuellement en stage dans le quelle je decouvre react native.",
+    ],
+    typeSpeed: 50,
+  });
+
+  return (
+    <div className="HomePage">
+      <img className="milas" src={image} alt="milas" />
+      <span className="intro">{text}</span>
+    </div>
+  );
 };
 
 export default HomePage;
