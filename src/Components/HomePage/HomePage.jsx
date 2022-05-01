6924122b49c23a0602cad98da422e3b7deff9076
SaliimH <sh.hemmar@gmail.com>
2022-05-01 02:39:28 +0200

    add section aboutme & start skills & change welcome img per gif

--- src/Components/HomePage/HomePage.jsx
+++ src/Components/HomePage/HomePage.jsx
@@ -1,66 +1,117 @@
 import React from "react";
 import Navbar from "../Navbar/NavBar";
-
+import Pppicture from "../../Assets/1641297370605.jpg";
 
 import "./HomePage.css";
 
 const HomePage = () => {
   return (
     <div className="ContainerHomepage">
+      <div className="header">
+        <Navbar />
+        <p className="txt-wl">
+          Welcome to Salim’s Portfolio scroll down to see The descrption{" "}
+        </p>
 
-<div className="header">
-<Navbar />
-<p className="txt-wl">Welcome to Salim’s Portfolio scroll down to see The descrption </p>
+        <svg class="arrows">
+          <path class="a1" d="M0 0 L30 32 L60 0"></path>
+          <path class="a2" d="M0 20 L30 52 L60 20"></path>
+          <path class="a3" d="M0 40 L30 72 L60 40"></path>
+        </svg>
+      </div>
+      <div className="cntr-bdy">
+      <div className="cntr-atme">
+        <div className="rectgl">
+          <p >
+            Je m'appelle Salim et je me suis énormément cherché.
+            <br />
+            Avoir un but fixe a été pour moi difficile, car très curieux de
+            nature.
+            <br />
+            j'aime apprendre pleins de nouvelles choses que cela me soit utile
+            ou non ; en parallèle, j'ai toujours été intéressé par le domaine
+            informatique et je me suis enfin lancé dans une formation en tant
+            que développeur web à la Wild code School.
+            <br />
+            Quoi de mieux que le métier de développeur web afin de découvrir
+            énormément de domaine et surtout de ne jamais cesser d'apprendre,
+            car ce monde est un monde en constante évolution.🚀
+          </p>
+        </div>
+        <div className="conttain-ppp">
+          <img className="ppp" src={Pppicture} alt="Salim's head" />
+          <p className="p1">ABOUT ME</p>{" "}
+        </div>
+      </div>
+      <p className="skll">SKILLS</p>
+      <div className="rectgl">
+          <p >
+            Je m'appelle Salim et je me suis énormément cherché.
+            <br />
+            Avoir un but fixe a été pour moi difficile, car très curieux de
+            nature.
+            <br />
+            j'aime apprendre pleins de nouvelles choses que cela me soit utile
+            ou non ; en parallèle, j'ai toujours été intéressé par le domaine
+            informatique et je me suis enfin lancé dans une formation en tant
+            que développeur web à la Wild code School.
+            <br />
+            Quoi de mieux que le métier de développeur web afin de découvrir
+            énormément de domaine et surtout de ne jamais cesser d'apprendre,
+            car ce monde est un monde en constante évolution.🚀
+          </p>
+        </div>
+      </div>
 
-<svg class="arrows">
-							<path class="a1" d="M0 0 L30 32 L60 0"></path>
-							<path class="a2" d="M0 20 L30 52 L60 20"></path>
-							<path class="a3" d="M0 40 L30 72 L60 40"></path>
-						</svg>
-</div>
-<div className="container">
-</div>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
-     <p>Salut tu est sur la hompage</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
+      <p>coucou</p>
     </div>
-    
   );
 };
 
